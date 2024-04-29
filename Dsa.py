204b15778bec89c95419491057b01d4793f8b741
Maroof Malik <146874434+maroofmalik1@users.noreply.github.com>
2024-04-30 00:49:04 +0530

    Create Dsa.py

---
+++ Dsa.py
@@ -0,0 +1,87 @@
+#bubble sort
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+#selection sort
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+#insertion sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+#merge sort
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+        
+        merge_sort(L)
+        merge_sort(R)
+        
+        i = j = k = 0
+        
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+            
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+            
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+    return arr
+
+#quick sort
+
+#heap sort
+
+#counting sort
+
+def counting_sort(arr):
+    max_num = max(arr)
+    min_num = min(arr)
+    range_of_elements = max_num - min_num + 1
+    count_arr = [0 for _ in range(range_of_elements)]
+    output = [0 for _ in range(len(arr))]
+
+    for num in arr:
+        count_arr[num - min_num] += 1
+
+    for i in range(1, len(count_arr)):
+        count_arr[i] += count_arr[i - 1]
+
+    for i in range(len(arr) - 1, -1, -1):
+        output[count_arr[arr[i] - min_num] - 1] = arr[i]
+        count_arr[arr[i] - min_num] -= 1
+
+    for i in range(len(arr)):
+        arr[i] = output[i]
+
+    return arr
